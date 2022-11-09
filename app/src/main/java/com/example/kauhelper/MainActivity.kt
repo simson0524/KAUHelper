8052263b9bff1d568c8b675ab1d7169953495231
Quintillion0113 <awcrgym@gmail.com>
2022-11-09 12:25:23 +0900

    이게 맞을까요

--- app/src/main/java/com/example/kauhelper/MainActivity.kt
+++ app/src/main/java/com/example/kauhelper/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.kauhelper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kauhelper.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
